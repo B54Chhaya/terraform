a428320f44bea3321c6394d635c9954282300db2
chhayab54 <chhaya.10@gmail.com>
2023-07-16 18:58:17 +0530

    git Push

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 variable "city" {}
+variable "state_name" {
+  
+}
 
 output "city_name" {
     value = "Our city name is ${var.city}"
+}
+
+output "state_name" {
+    value = "our state name is ${var.sate_name}
 }
